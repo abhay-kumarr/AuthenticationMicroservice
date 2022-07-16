27e3cd053475618e705a607496f7e3f64e41bd75
Abhay Kumar <67652179+abhay-kumarr@users.noreply.github.com>
2022-07-16 10:42:06 +0530

    Initial commit

---
+++ AuthenticationMicroservice/Helpers/API.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthenticationMicroservice.Helpers
+{
+    public static class API
+    {
+        public static class CustomerMicroservice
+        {
+            private const string BASE_URL = "https://localhost:44366/api/Customer/";
+
+            public static string ValidateUrl()
+            {
+                return new Uri(new Uri(BASE_URL), "validate").ToString();
+            }
+        }
+
+        public static class ManagerMicroservice
+        {
+            private const string BASE_URL = "https://localhost:44348/api/Manager/";
+
+            public static string ValidateExecutiveUrl()
+            {
+                return new Uri(new Uri(BASE_URL), "validateExecutive").ToString();
+            }
+
+            public static string ValidateManagerUrl()
+            {
+                return new Uri(new Uri(BASE_URL), "validateManager").ToString();
+            }
+        }
+    }
+}
---
+++ AuthenticationMicroservice/Services/AuthService.cs
@@ -0,0 +1,120 @@
+﻿using AuthenticationMicroservice.Helpers;
+using AuthenticationMicroservice.Models;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthenticationMicroservice.Services
+{
+    public interface IAuthService
+    {
+        public bool IsRoleValid(string role);
+        public bool IsUserValid(User user, out AuthUser authUser);
+        public string GenerateToken(AuthUser authUser);
+        public AuthUser VerifyToken(TokenString token);
+    }
+
+    public class AuthService : IAuthService
+    {
+        private const string SECRET_KEY = "secret-key-value--AHGTYytr7@hgf$GHF";
+
+        public bool IsRoleValid(string role)
+        {
+            return Role.Exists(role);
+        }
+
+        public bool IsUserValid(User user, out AuthUser authUser)
+        {
+            try
+            {
+                authUser = null;
+                string reqAddress = string.Empty;
+                switch (user.Role)
+                {
+                    case Role.Manager:
+                        reqAddress = API.ManagerMicroservice.ValidateManagerUrl();
+                        break;
+                    case Role.Executive:
+                        reqAddress = API.ManagerMicroservice.ValidateExecutiveUrl();
+                        break;
+                    case Role.Customer:
+                        reqAddress = API.CustomerMicroservice.ValidateUrl();
+                        break;
+                    default:
+                        break;
+                }
+                HttpResponseMessage response = new HttpResponseMessage();
+                HttpClientHandler clientHandler = new HttpClientHandler();
+                HttpContent content = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json");
+                clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+                using (var client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    response = client.PostAsync(reqAddress, content).Result;
+                    if ((int)response.StatusCode != 200) return false;
+                    authUser = JsonConvert.DeserializeObject<AuthUser>(response.Content.ReadAsStringAsync().Result);
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public string GenerateToken(AuthUser authUser)
+        {
+            try
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.ASCII.GetBytes(SECRET_KEY);
+                var tokenDescriptor = new SecurityTokenDescriptor()
+                {
+                    Subject = new ClaimsIdentity(new Claim[] { new Claim("id", authUser.Id.ToString()), new Claim("role", authUser.Role) }),
+                    Expires = DateTime.UtcNow.AddSeconds(2000),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+                };
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                return tokenHandler.WriteToken(token);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public AuthUser VerifyToken(TokenString tokenObj)
+        {
+            try
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.ASCII.GetBytes(SECRET_KEY);
+                tokenHandler.ValidateToken(tokenObj.Token, new TokenValidationParameters()
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken validatedToken);
+                var jwtToken = (JwtSecurityToken)validatedToken;
+                AuthUser authUser = new AuthUser();
+                authUser.Id = int.Parse(jwtToken.Claims.First(x => x.Type == "id").Value);
+                authUser.Role = jwtToken.Claims.First(x => x.Type == "role").Value;
+                return authUser;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
